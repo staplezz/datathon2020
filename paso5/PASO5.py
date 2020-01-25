85307bbc47c5014dd8e8164e9788e9e0f03c9645
Jorge Francisco Cortés López <kokofrank@ciencias.unam.mx>
2020-01-25 00:24:08 -0600

    Update PASO5.py

--- paso5/PASO5.py
+++ paso5/PASO5.py
@@ -1,16 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 24 19:32:40 2020
-
-@author: ilian
-"""
 import numpy as np
 import pandas as pd
 import io
 import networkx as nx
+import matplotlib.pyplot as plt
 
+#Leémos la gráficas del catálogo de aristas.
 df=pd.read_csv('edges.csv')
 tuples = [tuple(x) for x in df.to_numpy()]
+#Creamos la gráfica.
 G = nx.Graph()
 G.add_edges_from(tuples)
-print(G)
+
+#Función auxiliar que nos ayuda a obtener las subgráficas conexas de la gráfica.
+#Regresa una lista de gráficas conexas.
+def connected_component_subgraphs(G):
+    subgraphs_connected = []
+    for c in nx.connected_components(G):
+        subgraphs_connected.append(G.subgraph(c))
+
+    return subgraphs_connected
+
+#TEST.
+#Obtenemos las subgráficas.
+subg = connected_component_subgraphs(G)
+#Dibujamos una subgráfica aleatoria.
+nx.draw(subg[1],with_labels=True)
+plt.show()
